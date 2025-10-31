953f36d512b3b3cee60bad151f9377e2af162da3
Sigurd Holsen <sigurd.holsen@cognite.com>
2025-10-31 17:57:19 +0100

    @ and $

--- qmk/keymaps/default/keymap.c
+++ qmk/keymaps/default/keymap.c
@@ -87,8 +87,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                           _______, _______, _______, XXXXXXX,       MS_BTN1, MS_BTN2, _______, XXXXXXX
     ),
     [QUICK_NUMBERS] = LAYOUT(
-        _______, XXXXXXX,    KC_7,    KC_8,     KC_9,  XXXXXXX,     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-        _______,    KC_0,    KC_4,    KC_5,     KC_6,     KC_0,     XXXXXXX, XXXXXXX, MO(QUICK_SYMBOLS), XXXXXXX, XXXXXXX, XXXXXXX,
+        _______,RALT(KC_4),  KC_7,    KC_8,     KC_9,  XXXXXXX,     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+        _______,RALT(KC_2),  KC_4,    KC_5,     KC_6,     KC_0,     XXXXXXX, XXXXXXX, MO(QUICK_SYMBOLS), XXXXXXX, XXXXXXX, XXXXXXX,
         _______, XXXXXXX,    KC_1,    KC_2,     KC_3,   KC_DOT,     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
                            _______, _______, _______,  _______,     _______, _______, _______, _______
     ),
